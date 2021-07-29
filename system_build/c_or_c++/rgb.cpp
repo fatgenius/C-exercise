4bb02f0ed21183120045f29a5ca371b96bf493de
fatgenius <dbtlive@gmail.com>
2021-07-29 11:13:30 +0800

    C++ vs c

---
+++ system_build/c_or_c++/rgb.cpp
@@ -0,0 +1,22 @@
+/********************************
+ * some  basic C++ code to introduce the C++
+ * in C++ we could use enum to do lots of shits
+ * to improve our debugging  compare with c c++ is cool
+
+
+
+
+# include <iostream>
+using  namespace std;
+
+int main(){
+    enum class RGB { RED,GREEN, BLUE};
+    RGB color =  RGB::GREEN;
+    switch (color)
+    {
+    case RGB::GREEN: cout<<"green \n"; break;
+    case RGB::BLUE:  cout<<"BLUE \n"; break;
+    case RGB::RED:   cout<<"RED \n"; break;
+    }
+    return 0;
+}
